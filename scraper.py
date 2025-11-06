6c1752ae0643bb925f46345956ca1eab5c4c1035
nullcape32 <iulianchirita@ymail.com>
2025-11-06 22:00:10 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -58,8 +58,8 @@ CHANNEL_URLS = {
     "France24": {"url": "https://programtv.ro/canal-tv/france-24", "id": 74},
     "TVR Sport": {"url": "https://programtv.ro/canal-tv/tvr-sport", "id": 77},
     "Romania TV": {"url": "https://programtv.ro/canal-tv/romania-tv", "id": 78},
-    "AMC": {"url": "https://programtv.ro/canal-tv/amc", "id": 23}
-"DigiSport 2": {"url": "https://programtv.ro/canal-tv/digi-sport-2", "id": 91}
+    "AMC": {"url": "https://programtv.ro/canal-tv/amc", "id": 23},
+    "DigiSport 2": {"url": "https://programtv.ro/canal-tv/digi-sport-2", "id": 91}
 
 }
 
